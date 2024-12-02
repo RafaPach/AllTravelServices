bc0afb216c1f5efbd8f00df1cc162e931407fd01
RafaPach <rafael_eli_pacheco@hotmail.com>
2024-12-02 19:38:56 +0000

    request changes

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -27,7 +27,7 @@ function FooterComp() {
           ml: { xs: 0, sm: 1, md: 1, lg: 15, xl: 20 },
           mb: 3,
           width: {
-            xl: '300px',
+            xl: '320px',
             lg: '300px',
             md: '300px',
             sm: '27%',
